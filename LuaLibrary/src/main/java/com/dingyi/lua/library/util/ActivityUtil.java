249de87a2299a81d885de1ecc86587437982d50e
dingyi <2960586094@qq.com>
2021-04-19 03:32:38 +0800

    误删代码找回

---
+++ LuaLibrary/src/main/java/com/dingyi/lua/library/util/ActivityUtil.java
@@ -0,0 +1,21 @@
+package com.dingyi.lua.library.util;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+
+/**
+ * @author dingyi
+ * @date 2021/3/30 22:19
+ * 活动的工具类 提供一些工具方法
+ */
+public class ActivityUtil {
+    /**
+     *
+     * @param activity
+     * @return 返回根view的截图bitmap
+     */
+    public Bitmap getActivityScreen(Activity activity) {
+        return null;
+        //TODO
+    }
+}
