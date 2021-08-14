105f75edc1cbbdcf299b224412f37b549968546c
dingyi <dingyi222666@foxmail.com>
2021-08-15 02:10:51 +0800

    fix bug

--- editor/src/main/java/com/dingyi/editor/scheme/SchemeLua.kt
+++ editor/src/main/java/com/dingyi/editor/scheme/SchemeLua.kt
@@ -7,12 +7,9 @@ import io.github.rosemoe.editor.widget.EditorColorScheme
  * @date: 2021/8/15 0:41
  * @description:
  **/
-object SchemeLua: EditorColorScheme() {
+class SchemeLua: EditorColorScheme() {
 
 
-
-    const val STRING= 0xff53131
-    const val NAME= 0xff64622
     override fun applyDefault() {
         super.applyDefault()
         setColor(STRING,0xFFDD4488.toInt())
@@ -21,5 +18,10 @@ object SchemeLua: EditorColorScheme() {
         setColor(NAME,0xFF2A40FF.toInt())
         setColor(LITERAL, 0xFF6080FF.toInt())
         setColor(OPERATOR,0xFF808080.toInt())
+    }
+
+    companion object {
+        const val STRING= 0xff53131
+        const val NAME= 0xff64622
     }
 }
