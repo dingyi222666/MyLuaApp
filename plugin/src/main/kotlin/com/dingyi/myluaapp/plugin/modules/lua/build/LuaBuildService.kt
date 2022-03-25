04cd1b5b258c5ae1fa0cbf5b785e83cde5d78469
dingyi222666 <dingyi222666@foxmail.com>
2022-03-25 21:46:00 +0800

    fix: project template does not auto load androlua plugin

--- plugin/src/main/kotlin/com/dingyi/myluaapp/plugin/modules/lua/build/LuaBuildService.kt
+++ plugin/src/main/kotlin/com/dingyi/myluaapp/plugin/modules/lua/build/LuaBuildService.kt
@@ -4,8 +4,11 @@ import com.dingyi.myluaapp.build.api.Module
 import com.dingyi.myluaapp.build.api.Project
 import com.dingyi.myluaapp.build.api.builder.Builder
 import com.dingyi.myluaapp.build.api.service.HookService
+import com.dingyi.myluaapp.build.modules.android.config.BuildConfig
 import com.dingyi.myluaapp.build.modules.android.module.AndroidModule
 import com.dingyi.myluaapp.plugin.modules.lua.build.tasks.CompileLua
+import org.luaj.vm2.LuaBoolean
+import org.luaj.vm2.LuaNil
 import org.luaj.vm2.LuaTable
 
 class LuaBuildService:HookService {
@@ -32,11 +35,23 @@ class LuaBuildService:HookService {
         val builder = module
             .getBuilder()
 
-        builder.onInit {
-            builder.dependsOn(
-                CompileLua(module), builder
-                    .getTaskByName("MergeJavaResources")
-            )
+
+        val buildType = module
+            .getCache()
+            .getCache<BuildConfig>("${module.name}_build_config")
+            .buildVariants
+
+        val isCompileLua = module.getMainBuilderScript()
+            .get("android.buildTypes.$buildType.compileLua")
+
+        //default is true
+        if (isCompileLua is LuaNil || (isCompileLua is LuaBoolean && isCompileLua.booleanValue())) {
+            builder.onInit {
+                builder.dependsOn(
+                    CompileLua(module), builder
+                        .getTaskByName("MergeJavaResources")
+                )
+            }
         }
 
         return module
